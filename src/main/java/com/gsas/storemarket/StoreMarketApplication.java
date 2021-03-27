7d87d610a3fc19ea2354135005d9dcd3379a6286
victor flores <flores.vicf@gmail.com>
2021-03-26 23:51:21 -0500

    started proyect

---
+++ src/main/java/com/gsas/storemarket/StoreMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gsas.storemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsas/storemarket/StoreMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsas.storemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
